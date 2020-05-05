6a47cf4a8cf1546002ca79dd1ba3a2ae1c27ca37
piljae <piljae.chae>
2020-05-05 23:36:00 +0900

    absolute path to relative path

--- preprocess/npyLengthSaver.py
+++ preprocess/npyLengthSaver.py
@@ -6,12 +6,12 @@ import pickle
 import csv
 
 categories = ['Ballad', 'Rock', 'Pop', 'Hiphop', 'Trot', 'Dance', 'RnB']
-mapper = pd.ExcelFile('~/Downloads/mapper.xlsx')
+mapper = pd.ExcelFile('./resources/mapper.xlsx')
 
 def npyLengthSaver():
     musicFullName2NpyLength = {}
     for category in categories:
-        path = f"/Users/piljae/Desktop/SeparatedNpys/{category}"
+        path = f"./data/{category}"
         npys = os.listdir(path)
         for npy in tqdm(npys):
             fullpath = os.path.join(path, npy)
@@ -19,7 +19,7 @@ def npyLengthSaver():
             filename = os.path.splitext(basepath)[0]
             npyLength = len(np.load(fullpath))
             musicFullName2NpyLength[filename] = npyLength
-    with open('/Users/piljae/VoiceClassifier/music2npylength.pickle', 'wb') as f:
+    with open('./resources/music2npylength.pickle', 'wb') as f:
         pickle.dump(musicFullName2NpyLength, f, protocol=pickle.HIGHEST_PROTOCOL)
     
 if __name__ == '__main__':
